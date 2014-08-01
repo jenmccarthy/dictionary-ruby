4919e34b94f6528ad7526d3a0e44f940ba221620
jenmccarthy <jennifernward@gmail.com>
2014-08-01 15:48:41 -0700

    add get definition

--- dictionary.rb
+++ dictionary.rb
@@ -8,12 +8,15 @@ def main_menu
     puts "[== Dictionary ==]\n"
     puts "[c] Create a term and definition"
     puts "[l] List the words in your dictionary"
+    puts "[d] Get definitions of words"
     puts "[x] Exit"
     menu_choice = gets.chomp
     if menu_choice == "c"
       create_word
     elsif menu_choice == "l"
       list_words_with_index
+    elsif menu_choice == "d"
+      get_definition
     elsif menu_choice == "x"
       puts "Syonara"
       exit
@@ -40,6 +43,14 @@ def list_words_with_index
     puts "[#{index + 1}] #{collection.word}"
   end
   puts "\n"
+end
+
+def get_definition
+  list_words_with_index
+  puts "\nWhich word do you want to see?"
+  word_choice = gets.chomp.to_i
+  @current_term = Term.all[word_choice - 1]
+  puts "\n#{@current_term.word}: #{@current_term.definition}\n\n"
 end
 
 main_menu
